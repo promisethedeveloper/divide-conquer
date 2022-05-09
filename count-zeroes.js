740facbd8ffe89378bd109d4b8bec2e9aa9b3232
Promise Morka <onyekamorka06@gmail.com>
2022-05-09 19:42:14 -0400

    Initial commit

---
+++ count-zeroes.js
@@ -0,0 +1,26 @@
+function countZeroes(arr) {
+	// add whatever parameters you deem necessary - good luck!
+	let firstZero = findFirst(arr);
+	if (firstZero === -1) return 0;
+
+	return arr.length - firstZero;
+}
+
+function findFirst(arr, low = 0, high = arr.length - 1) {
+	if (high >= low) {
+		let mid = low + Math.floor((high - low) / 2);
+		// check if mid element is first 0
+		if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+			return mid;
+			// if mid element is not zero
+		} else if (arr[mid] === 1) {
+			return findFirst(arr, mid + 1, high);
+		} else {
+			// if mid element is zero but not first
+			return findFirst(arr, low, mid - 1);
+		}
+	}
+	return -1;
+}
+
+module.exports = countZeroes;
